7d07b9da21dcc96f911408d2c43456944724c80a
risei.sasaki <risei19980612@icloud.com>
2020-11-21 08:36:08 +0000

    favorite, unfavorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,9 @@ class User < ApplicationRecord
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
   
+  has_many :favorites, dependent: :destroy # dependent: :destroy はなくていい
+  has_many :likes, through: :favorites, source: :micropost
+  
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -29,5 +32,21 @@ class User < ApplicationRecord
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  # お気に入りするためのメソッド
+  def favorite(micropost)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  # お気に入り解除するためのメソッド
+  def unfavorite(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  # お気に入りしているかどうか確認のメソッド
+  def favorite?(micropost)
+    self.likes.include?(micropost)
   end
 end
